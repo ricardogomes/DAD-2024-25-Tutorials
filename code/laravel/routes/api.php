f86953d68659019ffc26109d57b3f017029a16a8
Ricardo Gomes <mail@ricardogomes.me>
2024-12-11 12:06:36 +0000

    feat: adding large database query examples

--- code/laravel/routes/api.php
+++ code/laravel/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +11,10 @@ Route::get('/users/me', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/auth/login', [AuthController::class, "login"]);
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::apiResource('games', GameController::class);
+    Route::apiResource('transactions', TransactionController::class);
+});
